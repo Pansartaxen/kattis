18c417090f46cb448d556d9d0a0d736908ce95f9
Marius Stokkedal <matk20@student.bth.se>
2022-01-25 17:30:29 +0100

    Update synchronisingLists.py

--- synchronisingLists.py
+++ synchronisingLists.py
@@ -9,10 +9,13 @@ while inpt != 0:
         for _ in range(inpt):
                 numberLst.append(int(input()))
         lst = orderLst#.sort()
+        print('---',lst)
         lst.sort()
+        print('---',lst)
         #outputLst = []
         for i in lst:
             index = orderLst.index(i)
-            print(numberLst[index])
+            print(i)
+            #print(numberLst[index])
         #€outputLst.append(numberLst[index])
     #print(outputLst)
