4e219d86f5b62c04d1f0d30c6c6e86e1392859fb
Marius Stokkedal <matk20@student.bth.se>
2022-09-04 21:53:01 +0200

    -

--- gcwvr.c
+++ gcwvr.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
+#include<string.h>
 /*#include <stdout.h>
 #include <stdin.h>*/
  
 int main(void) {
-    int gross = 0;
-    int truck = 0;
+    char gross = 0;
+    char truck = 0;
     int index = 0;
     char first_row[14] = {0};
-    gets( first_row );
+    scanf("%c",&first_row[14]);
     for(int i = 0; i < sizeof(first_row); i++){
-    if(first_row[i] != ' '){
+    if(&first_row[i] != " "){
         index++;
     }
-    else if (index == 0)
-    {
+    else if(&first_row[i] == "\n"){
+        break;
+    }
+    else{
         gross *= 10;
         gross += first_row[i];
     }
@@ -21,15 +24,15 @@ int main(void) {
 
 
     char second_row[400] = {0};
-    int count = 0;
-    int cur = 0;
-    gets(second_row);
+    char count = 0;
+    char cur = 0;
+    scanf("%c",&second_row[400]);
     for(int i = 0; i < sizeof(second_row); i++){
-        if(second_row[i] != ' '){
+        if(&second_row[i] != (char *)' '){
             count += cur;
             cur = 0;
         }
-        else if (second_row[i] >= 0 && second_row[i] <= 9)
+        else if (&second_row[i] >= (char *)0 && &second_row[i] <= (char *)9)
         /*Måste få in variabeln i jämförelsen utan att den läses bokstavligt*/
         {
             cur *= 10;
